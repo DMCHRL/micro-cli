763c69b73691fb2ba11b5e3d6c5a20dc964693d9
huangrunliang <1529730472@qq.com>
2021-07-29 16:01:33 +0800

    集成xxl-job

---
+++ xxl-job-sample/src/main/java/com/micro/saas/job/XxlJobSampleApplication.java
@@ -0,0 +1,13 @@
+package com.micro.saas.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class XxlJobSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobSampleApplication.class, args);
+    }
+
+}
